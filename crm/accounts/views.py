9e740dd0647978651cdf10c38671d5a0373179b8
Cristina Laryea <crispad06@gmail.com>
2020-04-23 00:54:27 -0400

    added request in navbar for user and admin and updated registerPage in views

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
 @unauthenticated_user
@@ -22,9 +23,13 @@ def registerPage(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            
+            group = Group.objects.get(name='Customer')
+            user.groups.add(group)
+            
+            messages.success(request, 'Account was created for ' + username)
             return redirect('login')
 
     context = {'form': form}
