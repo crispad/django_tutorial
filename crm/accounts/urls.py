0f7171b1982f6f5d32a2a22aad7fdb4351248afd
Cristina Laryea <crispad06@gmail.com>
2020-04-28 23:50:40 -0400

    created env and database using postgres

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view() name="password_reset_complete"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
 
 ]
